2cbfcbc3ff8269311f66b21880a0da317c543b4e
dnlksnvv <155080173+dnlksnvv@users.noreply.github.com>
2025-06-03 16:57:18 +0300

    feat(KAN-77): configure-nginx-and-docker-compose (#48)
    https://manukovskiy.atlassian.net/browse/KAN-77

--- backend/motivation_service/main.py
+++ backend/motivation_service/main.py
@@ -233,7 +233,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "main:app",
         host="0.0.0.0",
-        port=8008,
+        port=8007,
         reload=settings.DEBUG,
         log_level="info" if settings.DEBUG else "warning"
     ) 
